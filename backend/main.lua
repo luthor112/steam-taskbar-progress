269b59aad84706b8dbc501f8b9bbd13ff281c2c8
luthor112 <luthor112@gmail.com>
2026-01-02 10:04:12 +0100

    WIP: Moving to Lua

---
+++ backend/main.lua
@@ -0,0 +1,53 @@
+local logger = require("logger")
+local millennium = require("millennium")
+
+local MAX_PROGRESS = 100
+local completion_task = 0
+local custom_command = ""
+
+local function run_completion_task()
+    logger:info("Running task on completion")
+    if completion_task == 1 then
+        io.popen("shutdown /s /t 0")
+        completion_task = 0
+    elseif completion_task == 2:
+        io.popen(custom_command)
+        completion_task = 0
+    end
+end
+
+-- INTERFACES
+
+function set_progress_percent(percent)
+    logger:info("Progress at " .. percent)
+    -- TODO
+    return true
+end
+
+function set_completion_task(new_value, new_custom_command)
+    logger:info("Setting completion task mode to " .. new_value .. " with command " .. new_custom_command)
+    completion_task = new_value
+    custom_command = new_custom_command
+    return true
+end
+
+-- PLUGIN MANAGEMENT
+
+local function on_frontend_loaded()
+    logger:info("Frontend loaded")
+end
+
+local function on_load()
+    logger:info("Backend loaded")
+    millennium.ready()
+end
+
+local function on_unload()
+    logger:info("Unloading")
+end
+
+return {
+    on_frontend_loaded = on_frontend_loaded,
+    on_load = on_load,
+    on_unload = on_unload
+}
